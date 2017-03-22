d0cdbed0bc5b10d0241830d4ffc19b92e049ebcf
thomashines <thomashines@gmail.com>
2017-03-22 15:15:34 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -16,21 +16,7 @@ class CreateCostumeStores < ActiveRecord::Migration
   end
 end
 
-# class CreateCostumeStores < ActiveRecord::Migration
-#   def change
-#     create_table :costume_stores do |t|
-#       t.string :name
-#       t.integer :costume_inventory
-#       t.integer :num_of_employees
-#       t.boolean :still_in_business
-#       t.datetime :opening_time
-#       t.datetime :closing_time
-#
-#       t.string :location
-#       t.timestamps null: false
-#     end
-#   end
-# end
+
 
 # The `costume_stores` table will have seven columns:
 #   1. name
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -15,22 +15,7 @@ class CreateHauntedHouses < ActiveRecord::Migration
   end
 end
 
-# class CreateHauntedHouses < ActiveRecord::Migration
-#   def change
-#     create_table :haunted_houses do |t|
-#       t.string :name
-#       t.string :location
-#       t.string :theme
-#       t.float :price
-#       t.boolean :family_friendly
-#       t.datetime :opening_date
-#       t.datetime :closing_date
-#       t.text :description
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
+
 
 # The `haunted_houses` table will have eight columns:
 #   1. name
