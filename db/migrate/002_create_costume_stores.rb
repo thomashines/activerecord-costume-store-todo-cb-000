47f56e8cf493f7f0b625f540dd4d28806e959831
thomashines <thomashines@gmail.com>
2017-03-22 15:15:12 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,42 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+      t.string :location
+      t.timestamps null: false
+    end
+  end
+end
+
+# class CreateCostumeStores < ActiveRecord::Migration
+#   def change
+#     create_table :costume_stores do |t|
+#       t.string :name
+#       t.integer :costume_inventory
+#       t.integer :num_of_employees
+#       t.boolean :still_in_business
+#       t.datetime :opening_time
+#       t.datetime :closing_time
+#
+#       t.string :location
+#       t.timestamps null: false
+#     end
+#   end
+# end
+
+# The `costume_stores` table will have seven columns:
+#   1. name
+#   2. location
+#   3. number of costumes, or "costume inventory"
+#   4. number of employees
+#   5. whether or not it's still in business
+#   6. opening time
+#   7. closing time
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,43 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+      t.timestamps null: false
+    end
+  end
+end
+
+# class CreateHauntedHouses < ActiveRecord::Migration
+#   def change
+#     create_table :haunted_houses do |t|
+#       t.string :name
+#       t.string :location
+#       t.string :theme
+#       t.float :price
+#       t.boolean :family_friendly
+#       t.datetime :opening_date
+#       t.datetime :closing_date
+#       t.text :description
+#
+#       t.timestamps null: false
+#     end
+#   end
+# end
+
+# The `haunted_houses` table will have eight columns:
+#   1. name
+#   2. location
+#   3. theme
+#   4. price
+#   5. whether they're family friendly or not
+#   6. opening date
+#   7. closing date
+#   8. long description
